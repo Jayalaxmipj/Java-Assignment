04d5d4946b3ce8bdb12a2389345aad42fad7f3b5
Jayalaxmipj <73294925+Jayalaxmipj@users.noreply.github.com>
2021-02-10 23:25:05 +0530

    Assignment 3__Book details

---
+++ BookDetails.java
@@ -0,0 +1,26 @@
+package book_details;
+import java.util.Scanner;
+public class BookDetails {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Book b=new Book();
+		Engineering b1=new Engineering();
+		Scanner s=new Scanner(System.in);
+		System.out.println("enter book number");
+		b.setBookNo(s.nextInt());
+		System.out.println("enter book title");
+		b.setTitle(s.nextLine());
+		System.out.println("enter book author");
+		b.setAuthor(s.nextLine());
+		System.out.println("enter book price");
+		b.setPrice(s.nextFloat());
+		
+		b1.print(b);
+		
+		
+		
+
+	}
+
+}
